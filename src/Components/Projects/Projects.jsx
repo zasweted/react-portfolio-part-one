8423860399b1ddea5e49a511a55a2a6169e22bd0
zasweted <zaswet@gmail.com>
2025-02-08 12:16:39 +0200

    Adjusted number of posts to display

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -15,50 +15,53 @@ const Projects = () => {
             <div>
                 <ol className='projects-section__list'>
                     {
-                        projectsData.map((project) => (
-                            <li key={project.id}>
-                                <a href={project.project_url} target="_blank" rel="noopener noreferrer">
-                                    <div className="projects-section__list-item-container">
-                                        <header>
-                                            <img className='projects-section__image' loading="lazy" width="200" height="48" decoding="async" src={project.project_img} alt={project.title} />
-                                        </header>
-                                        <div className="projects-section__list-item-content">
-                                            <h3>
-                                                <div>
-                                                    {(() => {
-                                                        const titleWords = project.title.trim().split(" ");
-                                                        const lastWord = titleWords.pop(); // Extract the last word
-                                                        const mainTitle = titleWords.join(" "); // Join remaining words
-                                                        return (
-                                                            <>
-                                                                {mainTitle}{" "}
-                                                                <span className="last-word-icon">
-                                                                    {lastWord} <FiArrowUpRight className="projects-section__icon" />
-                                                                </span>
-                                                            </>
-                                                        );
-                                                    })()}
+                        projectsData
+                            .sort((a, b) => b.year - a.year)
+                            .slice(0, 10)
+                            .map((project) => (
+                                <li key={project.id}>
+                                    <a href={project.project_url} target="_blank" rel="noopener noreferrer">
+                                        <div className="projects-section__list-item-container">
+                                            <header>
+                                                <img className='projects-section__image' loading="lazy" width="200" height="48" decoding="async" src={project.project_img} alt={project.title} />
+                                            </header>
+                                            <div className="projects-section__list-item-content">
+                                                <h3>
+                                                    <div>
+                                                        {(() => {
+                                                            const titleWords = project.title.trim().split(" ");
+                                                            const lastWord = titleWords.pop(); // Extract the last word
+                                                            const mainTitle = titleWords.join(" "); // Join remaining words
+                                                            return (
+                                                                <>
+                                                                    {mainTitle}{" "}
+                                                                    <span className="last-word-icon">
+                                                                        {lastWord} <FiArrowUpRight className="projects-section__icon" />
+                                                                    </span>
+                                                                </>
+                                                            );
+                                                        })()}
+                                                    </div>
+                                                </h3>
+                                                <div className='projects-section__description'>
+                                                    <p>
+                                                        {project.short_description}
+                                                    </p>
+                                                    <ul>
+                                                        {
+                                                            project.build_with.map((tech, index) => (
+                                                                <li key={index}>
+                                                                    <div>{tech}</div>
+                                                                </li>
+                                                            ))
+                                                        }
+                                                    </ul>
                                                 </div>
-                                            </h3>
-                                            <div className='projects-section__description'>
-                                                <p>
-                                                    {project.short_description}
-                                                </p>
-                                                <ul>
-                                                    {
-                                                        project.build_with.map((tech, index) => (
-                                                            <li key={index}>
-                                                                <div>{tech}</div>
-                                                            </li>
-                                                        ))
-                                                    }
-                                                </ul>
                                             </div>
                                         </div>
-                                    </div>
-                                </a>
-                            </li>
-                        ))
+                                    </a>
+                                </li>
+                            ))
                     }
                 </ol>
                 <div className="projects-section__archive">
